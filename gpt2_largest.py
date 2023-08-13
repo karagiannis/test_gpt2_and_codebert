1ef4bebc67aa83a4c34cf36769ad89f57dce8fee
karagiannis <lasse.l.karagiannis@gmail.com>
2023-08-13 06:22:52 +0200

    largest gpt2 model

--- gpt2_largest.py
+++ gpt2_largest.py
@@ -1,5 +1,9 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_name = "gpt2"  # Model name for the largest GPT-2 model
+# Model name for the largest GPT-2 model
+model_name = "gpt2-xl"
+
+# Model loading from the largest GPT-2 model
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
