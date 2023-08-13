6520deac959e46c5ec29dea252c5ab31590a6878
karagiannis <lasse.l.karagiannis@gmail.com>
2023-08-13 05:41:43 +0200

    Comments added

--- gpt2_model.py
+++ gpt2_model.py
@@ -1,6 +1,27 @@
 # gpt2_model.py
+# This script sets up the GPT-2 model for text generation.
+
+# Import necessary classes from the Transformers library
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-model_name = "gpt2"  # This is the model name for the 124M parameter version of GPT-2
+# Define the model name to be used (in this case, "gpt2")
+model_name = "gpt2"
+
+# Initialize the tokenizer for the GPT-2 model
+# The tokenizer converts text into numerical tokens that the model can understand
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+# Initialize the GPT-2 model itself (Lightweight version with 124 million parameters)
+# GPT2LMHeadModel is a class for language modeling tasks (text generation)
 model = GPT2LMHeadModel.from_pretrained(model_name)
+
+# Note: The Hugging Face library provides the GPT-2 model and tokenizer for easy integration.
+# The model can be fine-tuned for specific tasks, and its outputs can be generated.
+# In this script, we're using the pre-trained GPT-2 model without any fine-tuning.
+
+# The 'fine_tuned_model' folder is typically created during the fine-tuning process,
+# when you train the GPT-2 model on a specific dataset. It contains the fine-tuned model
+# weights and configuration.
+
+# Developed by Hugging Face (https://huggingface.co/)
+
